ce5682c35454887a06ec597dc21fdf16eef0611d
Antonio Lindo <antoniolvitall@gmail.com>
2023-04-15 18:06:40 -0300

    finalizando tela de fim de jogo

--- secretword/src/components/GameOver.js
+++ secretword/src/components/GameOver.js
@@ -1,10 +1,12 @@
 import './GameOver.css';
 
-const GameOver = ({retry}) => {
+const GameOver = ({retry, score}) => {
   return (
     <div>
-      <h1>Game Over</h1>
-      <button onClick={retry}>Resetar jogo</button>
+      <h1>Fim do jogo! </h1>
+      <h2>A sua pontuação foi: <span>{score}</span></h2>
+      <p>Vamos jogar novamente?</p>
+      <button onClick={retry}>Jogar</button>
     </div>
   )
 }
